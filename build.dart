6a021ff97b91a7a2aebb395906c308f6af2155cb
Eric Taix <eric.taix@gmail.com>
2014-01-31 19:01:02 +0100

    Initial commit

---
+++ build.dart
@@ -0,0 +1,6 @@
+import 'package:polymer/builder.dart';
+        
+main(args) {
+  build(entryPoints: ['web/dart_infinite_scroll.html'],
+        options: parseOptions(args));
+}
---
+++ web/index.dart
@@ -0,0 +1,7 @@
+import 'package:polymer/polymer.dart';
+
+main() => initPolymer();
+
+loadNextPage(event, detail, sender) {
+  print('loading');
+}
